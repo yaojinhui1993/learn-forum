ebf87a448e17962d6ad9c895f112a65eb5b709b4
yaojinhui <yaojinhui1993@gmail.com>
2018-02-28 22:37:32 +0800

    Episoded 12. Validation Errors and Old Data

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -12,18 +12,43 @@
                 <div class="panel-body">
                     <form action="/threads" method="POST">
                         {{ csrf_field() }}
+
+                        <div class="form-group">
+                          <label for="channel_id">Choose a channel</label>
+                          <select class="form-control" name="channel_id" id="channel_id" selected>
+                            <option value="">Choose one...</option>
+                            @foreach($channels as $channel)
+                                <option 
+                                    value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected': '' }}>
+                                    {{ $channel->name }}
+                                </option>
+                            @endforeach
+                          </select>
+                        </div>
+
                         <div class="form-group">
                             <label for="title">Title</label>
-                            <input type="text"
-                                class="form-control" name="title" id="title" aria-describedby="title" placeholder="The thread Title">
+                            <input 
+                                type="text"
+                                class="form-control" 
+                                name="title" 
+                                id="title" 
+                                aria-describedby="title" 
+                                placeholder="The thread Title"
+                                value="{{ old('title') }}"
+                            >
                             </div>
         
                             <div class="form-group">
                             <label for="body">Body</label>
-                            <textarea class="form-control" name="body" id="body" rows="8"></textarea>
+                            <textarea class="form-control" name="body" id="body" rows="8">{{ old('body') }}</textarea>
                             </div>
         
+                        <div class="form-group">
                             <button type="submit" class="btn btn-primary">Create thread</button>
+                        </div>
+
+                        @include('layouts.errors')
                     </form>
                 </div>
             </div>
